7c5591f1bd1c7d3884a50c6951026f5369bc1297
danieljoose <daniel.jose.leite@ccc.ufcg.edu.br>
2022-08-10 03:14:53 -0300

    Add deploy back

--- src/main/java/br/com/ecocalc/services/UsuarioService.java
+++ src/main/java/br/com/ecocalc/services/UsuarioService.java
@@ -84,7 +84,7 @@ public class UsuarioService {
 
 		Usuario usuario = new Usuario();
         usuario.setNome(nome);
-        usuario.setSobrenome(nome);
+        usuario.setSobrenome(sobrenome);
         usuario.setEmail(email);
         usuario.setSenha(encodedSenha);
         usuario = usuarioRepository.save(usuario);
